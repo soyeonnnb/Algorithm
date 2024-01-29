b88993d1d457c13209b951b9c01e6eaf4861b2c4
Soyeon Kim <la28s5d@naver.com>
2024-01-29 21:52:48 +0900

    [Gold IV] Title: 직사각형과 쿼리, Time: 736 ms, Memory: 81012 KB -BaekjoonHub

--- 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.java
+++ 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.java
@@ -7,23 +7,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<Integer>[][] lists = new List[N+1][N+1];
-        for(int i=0;i<=N;i++) {
-            for(int j=0;j<=N;j++) lists[i][j] = Arrays.asList(new Integer[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        }
+        int[][][] arr = new int[N+1][N+1][11];
         StringTokenizer st;
         for(int i=1;i<=N;i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=1;j<=N;j++) {
                 int num = Integer.parseInt(st.nextToken());
-                lists[i][j].set(num, 1);
+                arr[i][j][num]++;
             }
         }
         for(int i=1;i<=N;i++) {
             for(int j=1;j<=N;j++) {
                 for(int k=1;k<=10;k++) {
-                    int num = lists[i-1][j].get(k) +lists[i][j-1].get(k) -lists[i-1][j-1].get(k) + lists[i][j].get(k);
-                    lists[i][j].set(k, num);
+                    arr[i][j][k] += arr[i-1][j][k] + arr[i][j-1][k] - arr[i-1][j-1][k];
                 }
             }
         }
@@ -37,7 +33,7 @@ public class Main {
             int y2 = Integer.parseInt(st.nextToken());
             int answer = 0;
             for(int k=1;k<=10;k++) {
-                if (lists[x2][y2].get(k) - lists[x2][y1-1].get(k) - lists[x1-1][y2].get(k) + lists[x1-1][y1-1].get(k) > 0) answer++;
+                if (arr[x2][y2][k] - arr[x2][y1-1][k] - arr[x1-1][y2][k] + arr[x1-1][y1-1][k] > 0) answer++;
             }
             sb.append(answer+"\n");
         }
