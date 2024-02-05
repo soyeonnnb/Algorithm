20ac38f201bdbae28c2436cdb8e9ea8e4768ec65
Soyeon Kim <la28s5d@naver.com>
2024-02-05 16:20:44 +0900

    [Gold IV] Title: 사냥꾼, Time: 624 ms, Memory: 59536 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.java
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.java
@@ -1,33 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    private static class Animal {
-        int x;
-        int y;
-        boolean checked;
-
-        public Animal(int x, int y) {
-            this.x = x;
-            this.y = y;
-            this.checked = false;
-        }
-
-//        @Override
-//        public String toString() {
-//            return "Animal[x="+x+", y="+y+", checked="+checked+"]";
-//        }
-
-        @Override
-        public String toString() {
-            return "["+x+", "+y+"]";
-        }
-
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -38,84 +15,32 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<M;i++) marr[i] = Integer.parseInt(st.nextToken());
         Arrays.sort(marr);
-//        System.out.println(Arrays.toString(marr));
-        Animal[] arr = new Animal[N];
+        int[][] arr =new int[N][2];
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr[i] = new Animal(x, y);
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr, (o1, o2) -> o1.x == o2.x ? o1.y - o2.y : o1.x - o2.x);
+        // 이번에는 사대를 정렬해서
+        // 해당 동물과 가장 가까운 사대 찾고,
+        // 그 사대에 범위내에 들어가는지 확인해보자 -> 왜냐면 사정거리가 같으니까 사대 가까운만큼 갈 수 있음.
         int answer = 0;
-        // 각각의 사대를 확인
-        for(int m=0;m<M;m++) {
-            for(int flag = -L;flag <= L;flag++) { // i가 x좌표가 됨
-                int i = marr[m] + flag;
-                int s = 0;
-                int e = N-1;
-                int xl = -1; // x_lower_bound
-                int xr = -1; // x_upper_bound
-                int yl = -1; // y_lower_bound
-                int yr = -1; // y_upper_bound
-                while(s<=e) {
-                    int mid = (s+e)/2;
-                    if (arr[mid].x < i) {
-                        s = mid+1;
-                    } else {
-                        e = mid-1;
-                        xl = mid;
-                    }
+        for(int[] animal : arr) {
+            int s = 0;
+            int e = M-1;
+            while(s<=e) {
+                int mid = (s+e)/2;
+                if (Math.abs(marr[mid] - animal[0]) + animal[1] <= L) {
+                    answer++;
+                    break;
                 }
-                s = 0;
-                e = N-1;
-                while(s<=e) {
-                    int mid = (s+e)/2;
-                    if (arr[mid].x <= i) {
-                        xr = mid;
-                        s = mid+1;
-                    } else {
-                        e = mid-1;
-                    }
-                }
-                if (xl == -1 || xr == -1) continue;
-                s = xl;
-                e = xr;
-                int go = L - Math.abs(flag);
-                while(s<=e) {
-                    int mid = (s+e)/2;
-                    if (arr[mid].y < -go) {
-                        s = mid+1;
-                    } else {
-                        e = mid-1;
-                        yl = mid;
-                    }
-                }
-
-                s = xl;
-                e = xr;
-                while(s<=e) {
-                    int mid = (s+e)/2;
-                    if (arr[mid].y <= go) {
-                        yr = mid;
-                        s = mid+1;
-                    } else {
-                        e = mid-1;
-                    }
-                }
-                if (yl == -1 || yr == -1) continue;
-//                System.out.println(i+" "+xl+" "+xr+"  ->   "+yl+" "+yr);
-                for(int j=yl;j<=yr;j++) {
-
-                    if (!arr[j].checked) {
-//                        System.out.println(arr[j]);
-                        answer++;
-                        arr[j].checked = true;
-                    }
+                if (marr[mid] < animal[0]) {
+                    s = mid+1;
+                } else {
+                    e = mid-1;
                 }
             }
         }
-//        System.out.println(Arrays.toString(arr));
         System.out.println(answer);
     }
 }
