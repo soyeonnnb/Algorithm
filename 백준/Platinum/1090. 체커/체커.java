195c0db3a72c60755f92a5730cebaf605a4e48ba
Soyeon Kim <la28s5d@naver.com>
2024-03-01 16:03:22 +0900

    [Platinum IV] Title: 체커, Time: 1148 ms, Memory: 118912 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.java
+++ 백준/Platinum/1090. 체커/체커.java
@@ -7,17 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N=Integer.parseInt(br.readLine());
-        List<int[]> list = new ArrayList<>();
+        int[][] arr = new int[N][2];
         StringTokenizer st;
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
-            list.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
         Set<Integer> xset = new TreeSet<>();
         Set<Integer> yset = new TreeSet<>();
         for(int i=0;i<N;i++) {
-            xset.add(list.get(i)[0]);
-            yset.add(list.get(i)[1]);
+            xset.add(arr[i][0]);
+            yset.add(arr[i][1]);
         }
         StringBuilder sb = new StringBuilder();
         sb.append(0+" ");
@@ -27,7 +28,7 @@ public class Main {
             for(Integer x : xset) {
                 for(Integer y : yset) {
                     queue.clear();
-                    for(int i=0;i<N;i++) queue.add(Math.abs(x - list.get(i)[0]) + Math.abs(y - list.get(i)[1]));
+                    for(int i=0;i<N;i++) queue.add(Math.abs(x - arr[i][0]) + Math.abs(y - arr[i][1]));
                     int temp = 0;
                     for(int i=0;i<n;i++) temp += queue.poll();
                     answer = Math.min(answer, temp);
