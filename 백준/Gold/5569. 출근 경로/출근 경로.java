682fba6760c63c62d3a1ff89a68d58e009e4399b
Soyeon Kim <la28s5d@naver.com>
2024-03-19 21:21:03 +0900

    [Gold IV] Title: 출근 경로, Time: 112 ms, Memory: 13568 KB -BaekjoonHub

--- 백준/Gold/5569. 출근 경로/출근 경로.java
+++ 백준/Gold/5569. 출근 경로/출근 경로.java
@@ -1,41 +1,23 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private static int W, H;
-    private static int[][][][] answer;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        W = sc.nextInt();
-        H = sc.nextInt();
-        answer = new int[W+1][H+1][2][2];
-        for(int i=1;i<=W;i++) for(int j=1;j<=H;j++) for(int k=0;k<2;k++) Arrays.fill(answer[i][j][k], -1);
-        for(int i=1;i<=W;i++) {
-            answer[i][1][0][0] = 0;
-            answer[i][1][0][1] = 1;
-            answer[i][1][1][0] = 0;
-            answer[i][1][1][1] = 0;
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        int[][][][] arr = new int[w+1][h+1][2][2]; // w, h, 현재방향, 방향 바꿧는지(0 -> 안바꿈, 1 -> 바꿈)
+        for(int i=1;i<=h;i++) arr[1][i][1][0] = 1;
+        for(int i=1;i<=w;i++) arr[i][1][0][0] = 1;
+        for(int i=2;i<=w;i++) {
+            for(int j=2;j<=h;j++) {
+                arr[i][j][0][0] = (arr[i-1][j][0][0] + arr[i-1][j][0][1])%100000;
+                arr[i][j][0][1] = arr[i-1][j][1][0];
+                arr[i][j][1][0] = (arr[i][j-1][1][0] + arr[i][j-1][1][1])%100000;
+                arr[i][j][1][1] = arr[i][j-1][0][0];
+            }
         }
-        for(int i=1;i<=H;i++) {
-            answer[1][i][1][0] = 0;
-            answer[1][i][1][1] = 1;
-            answer[1][i][0][0] = 0;
-            answer[1][i][0][1] = 0;
-        }
-
-        System.out.println((recur(W, H, 0, 0) + recur(W, H, 0, 1)+recur(W, H, 1, 0)+recur(W, H, 1, 1))%100000);
-    }
-    private static int[] dx = new int[]{-1, 0};
-    private static int[] dy = new int[]{0, -1};
-    private static int recur(int w, int h, int dire, int go) {
-        if (w < 1 || h < 1) return 0;
-        else if (answer[w][h][dire][go] != -1) return answer[w][h][dire][go]%100000;
-
-        if (go == 0) {
-            answer[w][h][dire][go] = recur(w + dx[dire], h + dy[dire], (dire+1)%2, 1)%100000;
-        } else {
-            answer[w][h][dire][go] = (recur(w + dx[dire], h + dy[dire], dire, 0) + recur(w + dx[dire], h + dy[dire], dire, 1))%100000;
-        }
-        return answer[w][h][dire][go]%100000;
+        int ans = 0;
+        for(int i=0;i<2;i++) for(int j=0;j<2;j++) ans += arr[w][h][i][j];
+        System.out.println(ans%100000);
     }
 }
