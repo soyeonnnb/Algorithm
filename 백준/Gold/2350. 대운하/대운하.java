2903046e8e4129defc7937d4fbe87ef3c64ff99d
Soyeon Kim <la28s5d@naver.com>
2024-03-22 20:17:54 +0900

    [Gold II] Title: 대운하, Time: 976 ms, Memory: 57456 KB -BaekjoonHub

--- 백준/Gold/2350. 대운하/대운하.java
+++ 백준/Gold/2350. 대운하/대운하.java
@@ -3,31 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 17:34 ~
+// 병렬 이진탐색 사용
 public class Main {
-    private static class Edge implements Comparable<Edge>{
-        int u, v, w;
-        Edge(int u, int v, int w) {
-            this.u = u;
-            this.v = v;
-            this.w = w;
-        }
-        @Override
-        public int compareTo(Edge e) {
-            return e.w - this.w;
-        }
-    }
-    private static class Destination implements Comparable<Destination> {
-        int dest, w;
-        Destination(int dest, int w) {
-            this.dest = dest;
-            this.w = w;
-        }
-        @Override
-        public int compareTo(Destination e) {
-            return e.w - this.w;
-        }
-    }
     private static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,65 +12,64 @@ public class Main {
         int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
         int K=Integer.parseInt(st.nextToken());
-
-        List<Destination>[] list = new ArrayList[N+1];
-        for(int i=0;i<=N;i++) list[i] = new ArrayList<>();
-        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        int[][] arr = new int[M][3];
         for(int i=0;i<M;i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            queue.add(new Edge(u, v, w));
+            for(int j=0;j<3;j++) arr[i][j] = Integer.parseInt(st.nextToken());
         }
-        parents = new int[N+1];
-        for(int i=0;i<=N;i++) parents[i] = i;
-        int connected = 0;
-        // edge 를 N-1개로 줄여줌
-        while(connected != N-1) {
-            Edge now = queue.poll();
-            if (union(now.u, now.v)) {
-                connected++;
-                list[now.u].add(new Destination(now.v, now.w));
-                list[now.v].add(new Destination(now.u, now.w));
-            }
-        }
-        PriorityQueue<Destination> destQueue = new PriorityQueue<>();
-        StringBuilder sb = new StringBuilder();
-        boolean[] visited = new boolean[N+1];
-        for(int k=1;k<=K;k++) {
+        // 운하의 폭을 이용해서 이진탐색을 할 것이므로 정렬!
+        Arrays.sort(arr, (o1, o2) -> o1[2] - o2[2]);
+
+        // 이진탐색을 할건데, 쿼리별로 한번에 묶어서 할 거니까!!
+        int[][] query = new int[K][2];
+        for(int i=0;i<K;i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            Arrays.fill(visited, false);
-            destQueue.clear();
-            visited[a] = true;
-            int answer = Integer.MAX_VALUE ;
-            for(Destination e : list[a]) destQueue.add(e);
-            while(!destQueue.isEmpty()) {
-                Destination now = destQueue.poll();
-                answer = Math.min(answer, now.w);
-                if(now.dest == b) break;
-                visited[now.dest] = true;
-                for(Destination e : list[now.dest]) {
-                    if (visited[e.dest]) continue;
-                    destQueue.add(e);
+            for(int j=0;j<2;j++) query[i][j] = Integer.parseInt(st.nextToken());
+        }
+        int[][] binary = new int[K][2]; // 이걸 이용해서 각 쿼리별로 s랑 e 저장
+        for(int i=0;i<K;i++) {
+            binary[i][0] = 1;
+            binary[i][1] = 200;
+        }
+        int[] answer = new int[K];
+        parents = new int[N+1];
+        // 계속 돌면서 확인할거임
+        List<int[]> list = new ArrayList<>();
+        while(true) {
+            for(int i=0;i<=N;i++) parents[i] = i;
+            list.clear();
+            for(int i=0;i<K;i++) {
+                if (binary[i][0] > binary[i][1]) continue; // 이미 이진탐색이 끝났으면
+                list.add(new int[]{(binary[i][0] + binary[i][1])/2, i}); // mid값이랑 query 번호랑 같이 저장
+            }
+            if (list.isEmpty()) break;
+            list.sort((o1, o2) -> o1[0] - o2[0]);
+            int idx = M-1; // 각 운하를 확인할거임
+            for(int i=list.size()-1;i>=0;i--) { // 쿼리를 보면서
+                // 만약 더 합쳐도 되면(현재 타겟하고 있는 운하가 내가 찾으려는 것보다 크면)
+                while (idx >= 0 && arr[idx][2] >= list.get(i)[0]) {
+                    union(arr[idx][0], arr[idx][1]); // 해당 도시를 연결하기
+                    idx--;
+                }
+                // 같은 집합이면(합쳐졌으면) 더 큰 운하도 가능한가 확인하면 됨
+                if (findset(query[list.get(i)[1]][0]) == findset(query[list.get(i)[1]][1])) {
+                    answer[list.get(i)[1]] = list.get(i)[0];
+                    binary[list.get(i)[1]][0] = list.get(i)[0] + 1;
+                } else { // 같은 집합이 아니면 더 작은 운하여야 함
+                    binary[list.get(i)[1]][1] = list.get(i)[0] - 1;
                 }
             }
-            System.out.println(answer);
         }
-        System.out.println(sb);
+        for(int x : answer) System.out.println(x);
     }
     private static int findset(int x) {
         if (parents[x] == x) return x;
         else return parents[x] = findset(parents[x]);
     }
-    private static boolean union(int x, int y) {
+    private static void union(int x, int y) {
         int rx = findset(x);
         int ry = findset(y);
-        if (rx == ry) return false;
-        else if (rx < ry) parents[ry] =rx;
+        if (rx < ry) parents[ry] = rx;
         else parents[rx] = ry;
-        return true;
     }
 }
