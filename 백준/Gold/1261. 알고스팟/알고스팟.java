e44c3d87184d696bca29605791071bcdb1a7d1b8
Soyeon Kim <la28s5d@naver.com>
2024-03-29 20:31:42 +0900

    [Gold IV] Title: 알고스팟, Time: 244 ms, Memory: 18644 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-// 20:20 ~
+// 20:20 ~ 20:29
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,21 +25,20 @@ public class Main {
         int[] dx = new int[]{-1, 1, 0, 0};
         int[] dy = new int[]{0, 0, -1, 1};
         int ans = N*M;
-        boolean[][] visited = new boolean[N][M];
-        visited[0][0] = true;
+        arr[0][0] = 2;
         outer: while(!pq.isEmpty()) {
             int[] now = pq.poll();
             if (now[2] >= ans) break;
             for(int k=0;k<4;k++) {
                 int nx = now[0] + dx[k];
                 int ny = now[1] + dy[k];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || arr[nx][ny] == 2) continue;
                 if (nx == N-1 && ny == M-1) {
                     ans = now[2];
                     break outer;
                 }
                 pq.add(new int[]{nx, ny, now[2] + arr[nx][ny]});
-                visited[nx][ny] = true;
+                arr[nx][ny] = 2;
             }
         }
         System.out.println(ans);
