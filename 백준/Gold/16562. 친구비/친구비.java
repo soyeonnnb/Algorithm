fc163ed83c6603fc9952009488187fae62eb3432
Soyeon Kim <la28s5d@naver.com>
2024-04-12 00:30:08 +0900

    [Gold IV] Title: 친구비, Time: 160 ms, Memory: 17132 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,53 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-// 21:49 ~
-// DFS로 풀어보자 !
+// 12:24
 public class Main {
-    private static int N, M, cost;
-    private static int[] arr;
-    private static boolean[] visited;
-    private static List<Integer>[] list;
+    private static int[] parent, price;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-        int k=Integer.parseInt(st.nextToken());
-        arr = new int[N+1];
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        int K=Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+        price = new int[N+1];
+        for(int i=0;i<=N;i++) parent[i] = i;
         st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=N;i++) arr[i] = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N+1];
-        for(int i=0;i<=N;i++) list[i] = new ArrayList<>();
-
+        for(int i=1;i<=N;i++) price[i] = Integer.parseInt(st.nextToken());
         for(int i=0;i<M;i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list[a].add(b);
-            list[b].add(a);
+            union(a, b);
         }
-        visited = new boolean[N+1];
-        int total = 0;
+        int ans = 0;
         for(int i=1;i<=N;i++) {
-            if (visited[i]) continue;
-            cost = arr[i];
-            dfs(i);
-            total += cost;
+            if (parent[i] == i) ans += price[i];
         }
-        if (total > k) System.out.println("Oh no");
-        else System.out.println(total);
+        if (ans <= K) System.out.println(ans);
+        else System.out.println("Oh no");
     }
-    private static void dfs(int now) {
-        visited[now] = true;
-        cost = Math.min(cost, arr[now]);
-        for(int num : list[now]) {
-            if (visited[num]) continue;
-            dfs(num);
-        }
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+    private static void union(int x, int y) {
+        int xr = find(x);
+        int yr = find(y);
+        if (xr == yr) return;
+        price[xr] = Math.min(price[xr], price[yr]);
+        parent[yr] = xr;
     }
 }
