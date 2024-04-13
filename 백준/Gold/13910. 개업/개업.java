0808232cd9e13e94cb131a28c48090c8baedefec
Soyeon Kim <la28s5d@naver.com>
2024-04-13 19:05:16 +0900

    [Gold V] Title: 개업, Time: 280 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.java
+++ 백준/Gold/13910. 개업/개업.java
@@ -1,46 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+// chat gpt 성능체크
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-// 15:22 ~
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
-        List<Integer> list = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        int[] visited = new int[N+1];
-        Arrays.fill(visited, -1);
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0;i<M;i++) {
-            int input = Integer.parseInt(st.nextToken());
-            list.add(input);
-            if (visited[input] != -1) continue;
-            queue.add(input);
-            visited[input] = 1;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[] dp = new int[20010];
+        List<Integer> v = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            int t = scanner.nextInt();
+            v.add(t);
         }
-        for(int i=0;i<M;i++) {
-            for(int j=0;j<M;j++) {
-                if (i == j) continue;
-                int sum = list.get(i) + list.get(j);
-                if (sum > N || visited[sum] != -1) continue;
-                queue.add(sum);
-                visited[sum] = 1;
-                list.add(sum);
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                if (i == j) {
+                    continue;
+                }
+                v.add(v.get(i) + v.get(j));
             }
         }
-        while(!queue.isEmpty()) {
-            int now = queue.poll();
-            for(int num : list) {
-                int sum = now + num;
-                if (sum > N || visited[sum] != -1) continue;
-                queue.add(sum);
-                visited[sum] = visited[now] + 1;
-            }
+
+        for (int x : v) {
+            dp[x] = 1;
         }
-        System.out.println(visited[N]);
+
+        for (int i = 1; i < 10010; i++) {
+            if (dp[i] != 0) {
+                continue;
+            }
+
+            for (int j = 1; j < i; j++) {
+                if (dp[j] == 0 || dp[i - j] == 0) {
+                    continue;
+                }
+
+                if (dp[i] == 0) {
+                    dp[i] = dp[j] + dp[i - j];
+                } else {
+                    dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
+                }
+
+            }
+
+        }
+        if (dp[N] == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[N]);
+        }
     }
 }
