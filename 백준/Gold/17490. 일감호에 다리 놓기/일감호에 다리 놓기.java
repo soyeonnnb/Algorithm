d678bf8135ba384de3dbdb6d2e11f7fa0c6f9387
Soyeon Kim <la28s5d@naver.com>
2024-05-07 14:42:52 +0900

    [Gold III] Title: 일감호에 다리 놓기, Time: 1268 ms, Memory: 347268 KB -BaekjoonHub

--- 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.java
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 // 14:07 ~
 public class Main {
@@ -25,7 +22,7 @@ public class Main {
         for(int i=0;i<=N;i++) parents[i] = i;
         st = new StringTokenizer(br.readLine());
         for(int i=1;i<=N;i++) arr[i] = Integer.parseInt(st.nextToken());
-        Set<Integer> construction = new TreeSet<>();
+        Set<Integer> construction = new HashSet<>();
         for(int i=0;i<M;i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
