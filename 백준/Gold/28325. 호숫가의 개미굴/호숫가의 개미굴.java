47ebe36d3268710a666e2823893bae37a2aa6b8c
Soyeon Kim <la28s5d@naver.com>
2024-06-02 21:11:50 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 384 ms, Memory: 79612 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
@@ -7,16 +7,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N=Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        long[] arr = new long[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<N;i++) arr[i] = Long.parseLong(st.nextToken());
 
         // 0은 해당 노드에 개미X, 1은 해당 노드에 개미O
         // N번째 , 해당 노드에 개미 유무, 0번 노드에 개미 유무
         long[][][] dp = new long[N][2][2];
         dp[0][0][0] = arr[0];
-        dp[0][1][0] = 0;
-        dp[0][0][1] = 0;
         dp[0][1][1] = 1;
         for(int i=1;i<N;i++) {
             for(int k=0;k<2;k++) {
@@ -24,14 +22,6 @@ public class Main {
             dp[i][1][k] = dp[i-1][0][k] + 1; //  해당 노드에 개미가 있으니까 앞에 없는 상태에서 1개 추가
             }
         }
-        // 마지막 노드는 양쪽 다 고려해야 함
-        // 1. 해당 노드에 개미가 있는 경우 -> 0이랑 N-2 둘다 없어야 함
-        long ans = dp[N-2][0][0] + 1; // 둘다 없음
-
-        // 2. 해당 노드에 개미가 없는 경우
-        // 양쪽 다 잇든 말든 상관 없음
-        ans = Math.max(ans, Math.max(Math.max(dp[N-2][0][0], dp[N-2][0][1]), Math.max(dp[N-2][1][0], dp[N-2][1][1])) + arr[N-1]);
-
-        System.out.println(ans);
+        System.out.println(Math.max(dp[N-1][0][0], Math.max(dp[N-1][0][1], dp[N-1][1][0])));
     }
 }
