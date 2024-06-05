d83fb64651e2ca8f0c193ea2f983e84945fce380
Soyeon Kim <la28s5d@naver.com>
2024-06-05 14:54:43 +0900

    [Silver IV] Title: 대칭 차집합, Time: 624 ms, Memory: 92468 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
@@ -11,7 +12,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
-        Set<Integer> set = new TreeSet<>();
+        Set<Integer> set = new HashSet<>();
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++) {
             set.add(Integer.parseInt(st.nextToken()));
