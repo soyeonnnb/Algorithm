d5ae346baf496590194d0eb61a62e514eee3c4fd
Soyeon Kim <la28s5d@naver.com>
2024-07-05 18:05:24 +0900

    [Gold II] Title: 피자판매, Time: 128 ms, Memory: 17352 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.java
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -17,50 +17,40 @@ public class Main {
         int[] arr2 = new int[M];
         for(int i=0;i<M;i++) arr2[i] = Integer.parseInt(br.readLine());
 
-        int[] prefix1 = new int[pizza+1];
-        int[] prefix2 = new int[pizza+1];
-
-        for(int i=0;i<N;i++) {
-            int now = arr1[i];
-            int j=i;
-            while(now <= pizza) {
-                prefix1[now]++;
-                j++;
-                if (j >= N) {
-                    j=0;
-                }
-                if (i == j) break;
-                if ((i > j && i == j+1) || (i == 0 && j == N-1)) break;
-                now += arr1[j];
-            }
-        }
-        for(int i=0;i<M;i++) {
-            int now = arr2[i];
-            int j=i;
-            while(now <= pizza) {
-                prefix2[now]++;
-                j++;
-                if (j >= M) {
-                    j=0;
-                }
-                if (i == j) break;
-                if ((i > j && i == j+1) || (i == 0 && j == M-1)) break;
-                now += arr2[j];
-            }
-        }
-        int sum = 0;
-        for(int i=0;i<N;i++) sum += arr1[i];
-        if (sum <= pizza) prefix1[sum]++;
-
-        sum = 0;
-        for(int i=0;i<M;i++) sum += arr2[i];
-        if (sum <= pizza) prefix2[sum]++;
+        int[] prefix1 = sum(N, pizza, arr1);
+        int[] prefix2 = sum(M, pizza, arr2);
 
         long ans = 0;
-        prefix1[0] = 1;
-        prefix2[0] = 1;
         for(int i=0;i<=pizza;i++) ans += (long) prefix1[i] * prefix2[pizza-i];
         System.out.println(ans);
 
     }
+
+    private static int[] sum(final int SIZE, final int PIZZA, int[] arr) {
+        int[] prefix = new int[PIZZA+1];
+        
+        prefix[0] = 1;
+
+        for(int i=0;i<SIZE;i++) {
+            int now = arr[i];
+            int j=i;
+            while(now <= PIZZA) {
+                prefix[now]++;
+                j++;
+                if (j >= SIZE) {
+                    j=0;
+                }
+                if (i == j) break;
+                if ((i > j && i == j+1) || (i == 0 && j == SIZE-1)) break;
+                now += arr[j];
+            }
+        }
+
+        int sum = 0;
+        for(int i=0;i<SIZE;i++) sum += arr[i];
+        if (sum <= PIZZA) prefix[sum]++;
+
+        return prefix;
+    }
+
 }
