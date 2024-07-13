da0a695d9528beb8e3dca5593a35b6ed03f4fe76
Soyeon Kim <la28s5d@naver.com>
2024-07-13 13:42:11 +0900

    [Bronze III] Title: 영수증, Time: 88 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        for(int i=0;i<9;i++) N -= sc.nextInt();
+        System.out.println(N);
+    }
+}
