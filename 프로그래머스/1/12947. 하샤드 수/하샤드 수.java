f45739f376038ce6f1be78ef2435919511abdcfb
Soyeon Kim <la28s5d@naver.com>
2024-07-30 15:10:31 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int temp = x;
+        while(temp > 0) {
+            sum += temp%10;
+            temp /= 10;
+        }
+        
+        if (x%sum == 0) return true;
+        else return false;
+    }
+}
