c34d4499d49fc0d5d22cea90664313d0aef96e90
Soyeon Kim <la28s5d@naver.com>
2025-05-06 20:52:03 +0900

    [Silver I] Title: 금민수의 개수, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
@@ -1,16 +1,19 @@
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     private static int[] arr;
     private static int ans, A, B;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         arr = new int[10];
-        A=sc.nextInt();
-        B=sc.nextInt();
+        A=Integer.parseInt(st.nextToken());
+        B=Integer.parseInt(st.nextToken());
         ans = 0;
         recur(0);
         System.out.println(ans);
