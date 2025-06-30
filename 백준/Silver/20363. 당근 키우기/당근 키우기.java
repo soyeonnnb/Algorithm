d5c5afc18342c495475f59964674ed0090ca7f14
Soyeon Kim <la28s5d@naver.com>
2025-06-30 21:34:10 +0900

    [Silver IV] Title: 당근 키우기, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/20363. 당근 키우기/당근 키우기.java
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        int Y = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
         System.out.println(X + Y + Math.min(X/10, Y/10));
     }
 }
